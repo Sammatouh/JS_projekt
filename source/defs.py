93959ca9e4bfa39f43295a4198bc077c2e5e84c2
Sammatouh <44528287+Sammatouh@users.noreply.github.com>
2020-04-23 12:09:34 +0200

    Add files via upload

---
+++ source/defs.py
@@ -0,0 +1,8 @@
+cannon_w, cannon_h = 65, 46
+angle_min, angle_max = 0, 60
+power_min, power_max = 30, 300
+left, right = 0, 1
+cannon_img_cols = 6
+transparent_color = (0, 0, 0)
+terrain_color = (50, 160, 80)
+base_color = (107, 110, 108)
